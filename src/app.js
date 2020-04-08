072423ca8b0f198ef73c7eeef137643248765fff
le.david993@gmail.com <le.david993@gmail.com>
2020-04-07 21:56:15 -0400

    WIP

--- src/app.js
+++ src/app.js
@@ -1,46 +1,23 @@
-// MIT License
-
-// Copyright (c) 2018 Neutralinojs
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-import {AppLib} from './app-core/lib';
-import './mycss.css';
-import './mycss2.css';
+import { AppLib } from "./app-core/lib";
+import React from "react";
+import ReactDOM from "react-dom";
 
 let appLib = new AppLib();
 
 let myapp = {
-    myfunction : function () { document.getElementById('info').innerHTML = NL_NAME + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + "<br/><br/>" + "<span>v" + NL_VERSION + "</span>"; }
+  myfunction: function () {
+    ReactDOM.render(
+      <div>Hello World My Friend</div>,
+      document.getElementById("app")
+    );
+  },
 };
-    
 
 Neutralino.init({
-    load: function() {
-        myapp.myfunction();
-        appLib.showSettings();
-    },
-    pingSuccessCallback : function() {
-
-    },
-    pingFailCallback : function() {
-
-    }
+  load: function () {
+    myapp.myfunction();
+    appLib.showSettings();
+  },
+  pingSuccessCallback: function () {},
+  pingFailCallback: function () {},
 });
